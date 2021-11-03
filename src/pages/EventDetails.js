30051a5eb76846c3ce3dd4612461a1be67ce1f54
pparam-2610 <pparam2610@gmail.com>
2021-11-04 03:02:11 +0530

    inetegrated user and event services

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useHistory, useRouteMatch } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams, useRouteMatch } from "react-router";
 import ShowMoreText from "react-show-more-text";
 import Modal from "react-modal";
 import { useAuth } from "../contexts/AppContext";
@@ -9,6 +9,8 @@ import "../styles/Eventpage.scss";
 
 // Assests
 import CodeWars from "../assets/Codewars.jpg";
+import IPL from "../assets/IPL.jpg";
+import VSM from "../assets/VSM.jpg";
 import Technical from "../assets/tech.png";
 import Cal from "../assets/calendar.jpg";
 import Prize from "../assets/prize.jpg";
@@ -21,55 +23,103 @@ import FaqSection from "../components/FaqSection";
 import RegistrationModal from "../components/RegistrationModal";
 import MobileNav from "../components/MobileNav";
 import DesktopNav from "../components/DesktopNav";
+import { eventService } from "../services/eventService";
 const Description = ({ event }) => {
   return (
     <>
-      {event.description.length > 200 ? (
-        <ShowMoreText lines={6}>{event.description}</ShowMoreText>
+      {event.Description.length > 200 ? (
+        <ShowMoreText lines={6}>{event.Description}</ShowMoreText>
       ) : (
-        event.description
+        event.Description
       )}
       <div className="section-1">
         <div className="event-type">
           <div className="event-img-container">
             <img src={Technical} alt="" />
           </div>
-          <div className="event-text">{event.category}</div>
+          <div className="event-text">{'Techincal'}</div>
         </div>
         <div className="event-date">
           <div className="event-img-container">
             <img src={Cal} alt="" />
           </div>
-          <div className="event-text">{event.date}</div>
+          <div className="event-text">{event.Date}</div>
         </div>
         <div className="event-prize">
           <div className="event-img-container">
             <img src={Prize} alt="" />
           </div>
-          <div className="event-text">{event.prizes}</div>
+          <div className="event-text">{event.isSingle?event.Fee.Single:event.Fee.Team}</div>
         </div>
       </div>
     </>
   );
 };
 
+const Rules = ({rules})=>{
+  console.log('Rules: ',rules);
+  return (
+    <>
+      {/* {rules.map(i=>{
+        // console.log('The rule is: ',rule);
+        
+          `${i})`<br/>
+        
+      })} */}
+      {
+        rules.map((rule,i)=>{
+          return(
+            <>
+            {i+1}) {rule}
+            <br/>
+            </>
+          )
+        })
+      }
+    </>
+  )
+}
+
+const map = {
+  ipl:{
+    Title: 'IPL Auction',
+    image:IPL
+  },
+  vsm:{
+    Title: 'Virtual Stock Market',
+    image:VSM
+  }
+}
+
 const EventDetails = ({ event }) => {
   const { jello } = useAuth();
-  console.log(jello);
+  // console.log(jello);
+  const {eventName} = useParams();
   const history = useHistory();
-  console.log(event);
+  console.log(eventName);
   const [tabState, setTabState] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
+  const [eventData,setEventData] = useState(null);
 
   function tabsContent() {
     console.log(tabState);
-    if (tabState === 0) return <Description event={event} />;
-    if (tabState === 1) return event.rules;
-    if (tabState === 2) return <FaqSection faq={event.faq} />;
+    if (tabState === 0) return <Description event={eventData} />;
+    if (tabState === 1) return <Rules rules = {eventData.rules}/>;
+    if (tabState === 2) return <FaqSection faq={eventData.faq} />;
   }
+
+  useEffect(async()=>{
+    
+    let event = await eventService.getEvent(map[eventName].Title);
+    console.log('The eventData is: ',event);
+    setEventData(event);
+  },[])
 
   return (
     <>
+    {
+      eventData?
+      <>
       <DesktopNav />
       <div className="event-container">
         <MobileNav />
@@ -80,7 +130,7 @@ const EventDetails = ({ event }) => {
           <div className="left-grid">
             <div className="event-details-grid">
               <div className="event-title-container">
-                <div className="event-title">Codatron++</div>
+                <div className="event-title">{eventData.Title}</div>
                 {/* <div className="event-subtitle">Technical</div> */}
                 {/* <div className="event-interested">
                 <ArrowBackIcon />
@@ -113,14 +163,14 @@ const EventDetails = ({ event }) => {
                     FAQ
                   </div>
                 </div>
-                <div className="event-content">{tabsContent(event)}</div>
+                <div className="event-content">{tabsContent(eventData)}</div>
               </div>
               <div className="event-register">
                 <div className="reg-amt">
                   <span>Registration: </span>
                   <div className="reg-text">
                     <span>Rs:</span>
-                    {event.registration}
+                    {eventData.isSingle?eventData.Fee.Single:eventData.Fee.Team}
                   </div>
                 </div>
                 <div className="reg-btn-container">
@@ -134,11 +184,14 @@ const EventDetails = ({ event }) => {
           </div>
           <div className="right-grid">
             <div className="img-container">
-              <img src={CodeWars} alt="" />
+              <img src={map[eventName].image} alt="" />
             </div>
           </div>
         </div>
       </div>
+    </>
+      :""
+    }
     </>
   );
 };
--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,7 +1,77 @@
-function getEvent(id) {
+import { refType } from "@mui/utils";
+import firebase from "firebase";
+
+const userRef = firebase.firestore().collection('Users');
+const eventRef = firebase.firestore().collection('Events');
+
+
+async function getEvent(name) {
   // Get event and
+  let item = [];
+  let querySnapShot = await eventRef.where('Title','==',name).get();
+  querySnapShot.forEach(doc=>{
+    // console.log('The user data is: ',doc.data())
+    item.push(doc.data());
+  })
+  return item[0];
 }
+
+async function getUser(emailId){
+  let item = [];
+  // return new Promise(resolve=>{
+
+  let querySnapShot = await userRef.where('email','==',emailId).get();
+  // console.log('The data is: ',querySnapShot)
+  querySnapShot.forEach(doc=>{
+    // console.log('The user data is: ',doc.data())
+    item.push(doc.data());
+  })
+  return item[0];
+  // return 1;
+}
+
+async function createUser(emailId,phoneNumber,name){
+  // console.log('The data is: ',emailId,phoneNumber,name);
+  // userRef.add({
+  //   college:"",
+  //   email:emailId,
+  //   name:name,
+  //   phoneNumber:phoneNumber,
+  // }).then(docRef=>{
+  //   console.log("Document written with ID: ", docRef.id);
+  // })
+  let newUserRef = userRef.doc();
+
+  // let querySnapShot = await newUserRef.set({
+  //   college:"",
+  //   email:emailId,
+  //   name:name,
+  //   phoneNumber:phoneNumber,
+  //   uid:newUserRef.id
+  // })
+  // console.log('The new user created in createUser: ',newUserRef.id);
+  let data = {
+    college:"",
+    email:emailId,
+    name:name,
+    phoneNumber:phoneNumber,
+    uid:newUserRef.id
+  }
+
+  //  newUserRef.set(data).then(user=>{
+
+    //  });
+    await newUserRef.set(data);
+    console.log('The new user creat createer: ',newUserRef.get());
+  let currUser = await (await newUserRef.get()).data();
+  console.log('Dard: ',currUser);
+  // let item = [];
+  return currUser;
+}
+
 
 export const eventService = {
   getEvent,
+  getUser,
+  createUser
 };
