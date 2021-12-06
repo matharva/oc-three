3354ef513b7952a6404a1e0499dda4dbb137ddfd
Atharva Mohite <atharvamohite20@gmail.com>
2021-12-06 21:40:19 +0530

    cleanup + half working

--- src/components/ThreeDModel/ThreeContainer.js
+++ src/components/ThreeDModel/ThreeContainer.js
@@ -1,17 +1,22 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useEffect } from "react";
 
 // Three imports
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
+import { MapControls, Stats } from "@react-three/drei";
+import { DragControls } from "three/examples/jsm/controls/DragControls";
 
 // 3 Model
 // import Model from "./Mandir";
-import Model from "./Man";
-import Model2 from "./Clg";
-import Model3 from "./Clg2";
-import Model4 from "./Clg3";
-import Model5 from "./Clg4";
+// import Model from "./Man";
+// import Model2 from "./Clg";
+// import Model3 from "./Clg2";
+// import Model4 from "./Clg3";
+// import Model5 from "./Clg4";
+// import Model6 from "./Clg5";
 // import Model from "./Final";
+// import Model from "./Compressed10_clg5";
+import Model from "./Clg5";
 
 // import Watch from "./Watchman";
 // import Compressed from "./Compressed";
@@ -27,38 +32,6 @@ import CameraControls from "camera-controls";
 
 CameraControls.install({ THREE });
 const randomPos = (min = 5, max = -5) => Math.random() * (max - min) + min;
-
-function Controls({
-  zoom,
-  focus,
-  pos = new THREE.Vector3(),
-  look = new THREE.Vector3(),
-}) {
-  const camera = useThree((state) => state.camera);
-  const gl = useThree((state) => state.gl);
-  const controls = useMemo(() => new CameraControls(camera, gl.domElement), []);
-  return useFrame((state, delta) => {
-    zoom ? pos.set(focus.x, focus.y, focus.z) : pos.set(-50, 0, 0);
-    // console.log(object)
-    // zoom ? pos.set(-20, 0, 0) : pos.set(-50, 0, 0);
-    // zoom ? look.set(focus.x, focus.y, focus.z - 0.2) : look.set(0, 0, 4);
-
-    state.camera.position.lerp(pos, 0.9);
-    state.camera.updateProjectionMatrix();
-    // console.log(delta);
-
-    // controls.setLookAt(
-    //   state.camera.position.x,
-    //   state.camera.position.y,
-    //   state.camera.position.z,
-    //   look.x,
-    //   look.y,
-    //   look.z,
-    //   true
-    // );
-    return controls.update(delta);
-  });
-}
 
 function Cloud({ momentsData, zoomToView }) {
   return momentsData.map(({ position, color }, i) => (
@@ -112,6 +85,90 @@ function Plane() {
     </mesh>
   );
 }
+const deg2rad = (degrees) => degrees * (Math.PI / 180);
+
+function Controls({
+  zoom,
+  focus,
+  pos = new THREE.Vector3(),
+  look = new THREE.Vector3(),
+}) {
+  const camera = useThree((state) => state.camera);
+  const gl = useThree((state) => state.gl);
+  const controls = useMemo(() => new CameraControls(camera, gl.domElement), []);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [mousePos, setMousePos] = useState([0, 0]);
+  const [cameraPos, setCameraPos] = useState(0);
+
+  useEffect(() => {
+    // var mousePos = [0, 0];
+    // var cameraPos = 0;
+
+    document.addEventListener("mousedown", onMouseDown, false);
+    function onMouseDown(event) {
+      // semouseDown = true;
+      // mousePos = [event.offsetX, event.offsetY];
+      // cameraPos = camera.position;
+      console.log("In mouse down");
+      setMouseDown(true);
+      setMousePos([event.offsetX, event.offsetY]);
+      setCameraPos(camera.position);
+    }
+    document.addEventListener("mouseup", onMouseUp, false);
+    function onMouseUp(event) {
+      console.log("In mouse up");
+      // mouseDown = false;
+      setMouseDown(false);
+    }
+    document.addEventListener("mousemove", onMouseMove, false);
+    function onMouseMove(event) {
+      if (mouseDown) {
+        console.log("In mouse over");
+        // scale factor takes into account the current FOV
+        let scale = Math.tan(((camera.fov / 2) * Math.PI) / 180) / 1.5;
+        let dx = mousePos[0] - event.offsetX;
+        let dz = mousePos[1] - event.offsetY;
+        let x = cameraPos.x + scale * dx;
+        let z = cameraPos.z - scale * dz;
+        camera.position.x = x;
+        camera.position.z = z;
+        // mousePos = [event.offsetX, event.offsetY];
+        // cameraPos = camera.position;
+        setMousePos([event.offsetX, event.offsetY]);
+        setCameraPos(camera.position);
+      }
+    }
+
+    controls.setLookAt(
+      camera.position.x,
+      camera.position.y,
+      camera.position.z,
+      100,
+      100,
+      40,
+      true
+    );
+  }, []);
+  return useFrame((state, delta) => {
+    // zoom ? pos.set(focus.x, focus.y, focus.z) : pos.set(-50, 0, 0);
+    // console.log("pritam");
+    // // zoom ? pos.set(-20, 0, 0) : pos.set(-50, 0, 0);
+    // // zoom ? look.set(focus.x, focus.y, focus.z - 0.2) : look.set(0, 0, 4);
+    // state.camera.position.lerp(pos, 0.1);
+    // state.camera.updateProjectionMatrix();
+    // // console.log(delta);
+    // controls.setLookAt(
+    //   state.camera.position.x,
+    //   state.camera.position.y,
+    //   state.camera.position.z,
+    //   look.x,
+    //   look.y,
+    //   look.z,
+    //   true
+    // );
+    // return controls.update(delta);
+  });
+}
 
 const ThreeContainer = () => {
   const [zoom, setZoom] = useState(false);
@@ -140,24 +197,15 @@ const ThreeContainer = () => {
 
   return (
     <Canvas>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
+      <MapControls />
       <Stars />
       <spotLight position={[100, 150, 100]} angle={0.3} />
       <ambientLight intensity={0.5} />
       <Physics>
-        <Model5 />
+        {/* <Model /> */}
         {/* <Model2 /> */}
-        {/* <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Plane /> */}
-        {console.log("Before loading model")}
+        <Box />
         {/* <Model
           zoomToView={(focusRef) => {
             console.log(focusRef);
@@ -167,14 +215,7 @@ const ThreeContainer = () => {
         /> */}
 
         <Controls zoom={zoom} focus={focus} />
-        {/* <Watch /> */}
-        {/* <Compressed /> */}
-        {/* <CollegeBuilding /> */}
-        {console.log("After loading model")}
       </Physics>
-      {/* <Suspense fallback={null}> */}
-      {/* <Box /> */}
-      {/* </Suspense> */}
     </Canvas>
   );
 };
